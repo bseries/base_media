781e98a83b64c584f25551cbdf181ca31cb0495b
David Persson <davidpersson@gmx.de>
2013-09-25 10:05:45 +0200

    Adding base js.

---
+++ assets/js/base.js
@@ -0,0 +1,8 @@
+requirejs.config({
+  paths: {
+    'media-explorer-modal': 'media/js/media-explorer-modal',
+    'media-attachment': 'media/js/media-attachment',
+    'media-explorer': 'media/js/media-explorer/media-explorer'
+  }
+});
+
