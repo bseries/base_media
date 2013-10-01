275a7531c4b20fb8b201b4d41c9b31110387d8e7
David Persson <davidpersson@gmx.de>
2013-10-01 17:56:35 +0200

    Improve lightbox.

--- assets/js/media-lightbox.js
+++ assets/js/media-lightbox.js
@@ -1,31 +1,54 @@
 define('media-lightbox',
-['jquery', 'modal', 'domready!'],
-function($, Modal) {
+['jquery', 'modal', 'underscore', 'domready!'],
+function($, Modal, _) {
 
   Modal.init();
 
+  var _attach = function(el) {
+    el.addClass('media-lightboxed');
+
+    el.on('click', function(ev) {
+      Modal.loading();
+
+      ev.preventDefault();
+      var content = el.clone();
+      content.removeClass('media-lightboxed');
+
+      Modal.elements.content.one('click', Modal.close);
+      Modal.fill(content, 'media-lightbox');
+
+      Modal.ready();
+    });
+  };
+
+  var _detach = function(el) {
+    el.off('click');
+    el.removeClass('media-lightboxed');
+  };
+
+  var _checkSize = function(el) {
+    var natural = el.get(0).naturalWidth * el.get(0).naturalHeight;
+    var current = el.width() * el.height();
+
+    return natural > current;
+  };
+
   var make = function(selector) {
-      $(selector).each(function(k, el) {
-        el = $(el);
+    $(selector).each(function(k, el) {
+      el = $(el);
 
-        var natural = el.get(0).naturalWidth * el.get(0).naturalHeight;
-        var current = el.width() * el.height();
-
-        if (natural <= current) {
-          return;
+      $(window).on('resize', _.debounce(function() {
+        if (_checkSize(el)) {
+          _attach(el);
+        } else {
+          _detach(el);
         }
-        el.addClass('media-lightboxed');
+      }, 500));
 
-        el.on('click', function(ev) {
-        Modal.loading();
-          ev.preventDefault();
-          var content = el.clone();
-          content.removeClass('media-lightboxed');
-
-          Modal.fill(content, 'media-lightbox');
-          Modal.ready();
-        });
-      });
+      if (_checkSize(el)) {
+        _attach(el);
+      }
+    });
   };
 
   return {
