7dc746dd07ab3fbf602baed7ddbd5ca15e8072e4
David M. Persson <davidpersson@gmx.de>
2014-04-01 17:38:20 +0200

    Rename paths.

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,9 @@
 requirejs.config({
   paths: {
-    'media-explorer-modal': 'media/js/media-explorer-modal',
-    'media-attachment': 'media/js/media-attachment',
-    'media-explorer': 'media/js/media-explorer',
-    'media-lightbox': 'media/js/media-lightbox'
+    'media-explorer-modal': 'cms-media/js/media-explorer-modal',
+    'media-attachment': 'cms-media/js/media-attachment',
+    'media-explorer': 'cms-media/js/media-explorer',
+    'media-lightbox': 'cms-media/js/media-lightbox'
   },
   shim: {}
 });
