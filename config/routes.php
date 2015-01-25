3aa4dfca94c343326d156bfe1f6a9bc30c303638
David Persson <davidpersson@gmx.de>
2015-01-25 14:09:13 +0100

    Remove now generic routes.

--- config/routes.php
+++ config/routes.php
@@ -14,58 +14,14 @@ use lithium\net\http\Router;
 
 $persist = ['persist' => ['admin', 'controller']];
 
-// media
-Router::connect(
-	'/admin/api/media/{:id:([0-9]+|__ID__)}',
-	['controller' => 'media', 'action' => 'api_view', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
 Router::connect(
 	'/admin/api/media/page:{:page}',
-	['controller' => 'media', 'action' => 'api_index', 'library' => 'base_media', 'admin' => true],
+	['controller' => 'media', 'action' => 'index', 'library' => 'base_media', 'admin' => true, 'api' => true],
 	$persist
 );
 Router::connect(
 	'/admin/api/media/search/{:q}/page:{:page}',
-	['controller' => 'media', 'action' => 'api_search', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
-Router::connect(
-	'/admin/api/media/transfer-preflight',
-	['controller' => 'media', 'action' => 'api_transfer_preflight', 'library' => 'base_media', 'admin' => true]
-);
-Router::connect(
-	'/admin/api/media/transfer-meta',
-	['controller' => 'media', 'action' => 'api_transfer_meta', 'library' => 'base_media', 'admin' => true]
-);
-Router::connect(
-	'/admin/api/media/transfer',
-	['controller' => 'media', 'action' => 'api_transfer', 'library' => 'base_media', 'admin' => true]
-);
-
-Router::connect(
-	'/admin/media/transfer',
-	['controller' => 'media', 'action' => 'transfer', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
-Router::connect(
-	'/admin/media',
-	['controller' => 'media', 'action' => 'index', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
-Router::connect(
-	'/admin/media/regenerate-versions/{:id:[0-9]+}',
-	['controller' => 'media', 'action' => 'regenerate_versions', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
-Router::connect(
-	'/admin/media/{:action}/{:id:[0-9]+}',
-	['controller' => 'media', 'library' => 'base_media', 'admin' => true],
-	$persist
-);
-Router::connect(
-	'/admin/media/{:action}/{:args}',
-	['controller' => 'media', 'library' => 'base_media', 'admin' => true],
+	['controller' => 'media', 'action' => 'search', 'library' => 'base_media', 'admin' => true, 'api' => true],
 	$persist
 );
 
