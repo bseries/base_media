b0a8b8d0a071c9b10626fe2ab6983ee334d44dfa
David Persson <davidpersson@gmx.de>
2015-03-24 14:44:47 +0100

    Adding dummy command.

--- extensions/command/Media.php
+++ extensions/command/Media.php
@@ -13,6 +13,7 @@
 namespace base_media\extensions\command;
 
 use base_media\models\Media as MediaModel;
+use base_media\models\MediaVersions;
 
 class Media extends \lithium\console\Command {
 
@@ -36,6 +37,71 @@ class Media extends \lithium\console\Command {
 			$this->out($item->verify() ? 'OK' : 'FAILED');
 		}
 		$this->out('COMPLETED');
+	}
+
+	public function dummy() {
+		$this->out('Adding dummy transfers and versions.');
+
+		MediaModel::find('all', [
+			'conditions' => [
+				'title' => 'DUMMY'
+			]
+		])->delete();
+
+		ini_set('memory_limit','500M');
+		MediaVersions::find('all')->delete();
+
+		copy(
+			PROJECT_PATH . '/assets/app/img/dummy/01.jpg',
+			$file = PROJECT_MEDIA_FILE_BASE . '/' . uniqid('dummy_') . '.jpg'
+		);
+
+		$image = MediaModel::create([
+			'url' => $file,
+			'title' => 'DUMMY'
+		]);
+		if (!$image->save()) {
+			return false;
+		}
+		if (!$image->makeVersions()) {
+			return false;
+		}
+
+		$this->out('Replacing originals with dummy...');
+		foreach (MediaModel::all() as $item) {
+
+			if ($item->id === $image->id) {
+				continue; // Do not dummy or dummy.
+			}
+			$this->out("Processing item {$item->id}...");
+
+			if ($item->can('delete')) {
+				$item->deleteUrl();
+			}
+			$item->deleteVersions();
+
+			$item->url = $image->url;
+			$item->save([
+				'url' => $image->url,
+				'type' => $image->type,
+				'checksum' => $image->checksum,
+				'mime_type' => $image->mime_type
+			], ['callbacks' => false]);
+
+			foreach ($image->versions() as $version) {
+				$v = MediaVersions::create([
+					'media_id' => $item->id,
+					'version' => $version->version,
+					'url' => $version->url,
+					'type' => $version->type,
+					'checksum' => $version->checksum,
+					'mime_type' => $version->mime_type,
+					'status' => 'processed'
+				]);
+				$v->save(null, ['callbacks' => false]);
+			}
+		}
+		$this->out('COMPLETED, you may need to clear caches to make new media take effect.');
 	}
 }
 
