ad6640a3963b22fb91c8fdc49dc9e0b0bcb94a2a
David Persson <davidpersson@gmx.de>
2015-04-12 09:38:34 +0200

    Update schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,30 +1,4 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: localhost (MySQL 10.0.10-MariaDB-log)
-# Datenbank: rainmap
-# Erstellungsdauer: 2014-06-03 15:27:54 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
-# Export von Tabelle media
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media`;
-
+-- Create syntax for TABLE 'media'
 CREATE TABLE `media` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
@@ -39,13 +13,7 @@ CREATE TABLE `media` (
   KEY `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Unique index set on user_id+checksum instead of user_id+dirn';
 
-
-
-# Export von Tabelle media_attachments
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media_attachments`;
-
+-- Create syntax for TABLE 'media_attachments'
 CREATE TABLE `media_attachments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(11) unsigned NOT NULL,
@@ -56,13 +24,7 @@ CREATE TABLE `media_attachments` (
   KEY `media_file` (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-
-# Export von Tabelle media_versions
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `media_versions`;
-
+-- Create syntax for TABLE 'media_versions'
 CREATE TABLE `media_versions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(11) unsigned NOT NULL,
@@ -71,19 +33,10 @@ CREATE TABLE `media_versions` (
   `type` varchar(100) NOT NULL,
   `mime_type` varchar(100) NOT NULL,
   `checksum` char(32) DEFAULT '',
+  `status` varchar(20) NOT NULL DEFAULT 'unknown',
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user` (`media_id`),
   KEY `media_id` (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Unique index set on user_id+checksum instead of user_id+dirn';
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
