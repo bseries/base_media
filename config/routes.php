98aa0baa4b3418548cb33f1d97fe8b78f76b7379
David Persson <davidpersson@gmx.de>
2015-10-07 11:39:44 +0200

    Use scoped routes.

--- config/routes.php
+++ config/routes.php
@@ -20,56 +20,58 @@ namespace base_media\config;
 use lithium\net\http\Router;
 use base_core\extensions\net\http\ClientRouter;
 
-$persist = ['admin', 'controller'];
-$defaults = ['type' => 'json'];
+Router::scope('admin', function() {
+	$persist = ['admin', 'controller'];
+	$defaults = ['type' => 'json'];
 
-$base = ['controller' => 'media', 'library' => 'base_media', 'admin' => true, 'api' => true];
+	$base = ['controller' => 'media', 'library' => 'base_media', 'admin' => true, 'api' => true];
 
-Router::connect(
-	'/admin/api/base-media/media',
-	$base + ['action' => 'index'],
-	compact('persist', 'defaults')
-);
-Router::connect(
-	'/admin/api/base-media/media/page:{:page:(\d+|__PAGE__)}',
-	$base + ['action' => 'index'],
-	compact('persist', 'defaults')
-);
-Router::connect(
-	'/admin/api/base-media/media/search/{:q}/page:{:page:(\d+|__PAGE__)}',
-	$base + ['action' => 'search'],
-	compact('persist', 'defaults')
-);
-Router::connect(
-	'/admin/api/base-media/media/transfer/title:{:title}',
-	$base + ['action' => 'transfer'],
-	compact('persist', 'defaults')
-);
+	Router::connect(
+		'/api/base-media/media',
+		$base + ['action' => 'index'],
+		compact('persist', 'defaults')
+	);
+	Router::connect(
+		'/api/base-media/media/page:{:page:(\d+|__PAGE__)}',
+		$base + ['action' => 'index'],
+		compact('persist', 'defaults')
+	);
+	Router::connect(
+		'/api/base-media/media/search/{:q}/page:{:page:(\d+|__PAGE__)}',
+		$base + ['action' => 'search'],
+		compact('persist', 'defaults')
+	);
+	Router::connect(
+		'/api/base-media/media/transfer/title:{:title}',
+		$base + ['action' => 'transfer'],
+		compact('persist', 'defaults')
+	);
 
-ClientRouter::provide('media:view-batch',
-	$base + ['action' => 'index']
-);
+	ClientRouter::provide('media:view-batch',
+		$base + ['action' => 'index']
+	);
 
-ClientRouter::provide('media:index',
-	$base + ['action' => 'index', 'page' => '__PAGE__']
-);
-ClientRouter::provide('media:search',
-	$base + ['action' => 'search', 'q' => '__Q__', 'page' => '__PAGE__']
-);
-ClientRouter::provide('media:view',
-	$base + ['action' => 'view', 'id' => '__ID__']
-);
-ClientRouter::provide('media:transfer-preflight',
-	$base + ['action' => 'transfer_preflight']
-);
-ClientRouter::provide('media:transfer-meta',
-	$base + ['action' => 'transfer_meta']
-);
-ClientRouter::provide('media:transfer',
-	$base + ['action' => 'transfer', 'title' => '__TITLE__']
-);
-ClientRouter::provide('media:capabilities',
-	$base + ['action' => 'capabilities']
-);
+	ClientRouter::provide('media:index',
+		$base + ['action' => 'index', 'page' => '__PAGE__']
+	);
+	ClientRouter::provide('media:search',
+		$base + ['action' => 'search', 'q' => '__Q__', 'page' => '__PAGE__']
+	);
+	ClientRouter::provide('media:view',
+		$base + ['action' => 'view', 'id' => '__ID__']
+	);
+	ClientRouter::provide('media:transfer-preflight',
+		$base + ['action' => 'transfer_preflight']
+	);
+	ClientRouter::provide('media:transfer-meta',
+		$base + ['action' => 'transfer_meta']
+	);
+	ClientRouter::provide('media:transfer',
+		$base + ['action' => 'transfer', 'title' => '__TITLE__']
+	);
+	ClientRouter::provide('media:capabilities',
+		$base + ['action' => 'capabilities']
+	);
+});
 
 ?>
