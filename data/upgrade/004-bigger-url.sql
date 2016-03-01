e6cc8ab7d85dee146480b126bcc4280a7e11ce54
David Persson <davidpersson@gmx.de>
2016-03-01 15:40:06 +0100

    Increase url field size.

---
+++ data/upgrade/004-bigger-url.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `media` CHANGE `url` `url` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
+ALTER TABLE `media_versions` CHANGE `url` `url` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
