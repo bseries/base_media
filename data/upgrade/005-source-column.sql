b541098463feaf582504ab45dee5dba0ba573c63
David Persson <davidpersson@gmx.de>
2017-01-12 15:01:32 +0100

    Revert addition of migration.

--- data/upgrade/005-source-column.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `media` ADD `source` VARCHAR(250)  NULL  DEFAULT NULL  AFTER `checksum`;
-update media set source = 'admin' where owner_id in (select id from users where role = 'admin');
-
