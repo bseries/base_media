d2b7ce1b0102ed32b49c88aca50b65ca8df965a3
Marius Wilms <mariuswilms@mailbox.org>
2018-07-30 16:02:53 +0200

    Retrieve titles from dbtg.tv URLs correctly

--- models/RemoteMedia.php
+++ models/RemoteMedia.php
@@ -138,7 +138,8 @@ class RemoteMedia extends \base_core\models\Base {
 				'title' => function($url) {
 					$ch = curl_init();
 					curl_setopt($ch, CURLOPT_URL, $url);
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 					$result = curl_exec($ch);
 					curl_close($ch);
 
