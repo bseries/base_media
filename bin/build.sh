db09ddaf8c68518a3459528f2f4130e8e009d89f
Marius Wilms <mariuswilms@mailbox.org>
2018-12-03 13:06:36 +0100

    build.sh to not install npm packages

--- bin/build.sh
+++ bin/build.sh
@@ -23,16 +23,6 @@ last 2 Firefox versions
 last 2 Safari versions
 EOF
 
-# Babel cli can be installed locally, but presets are always search locally.
-cat << EOF > package.json
-{
-	"devDependencies": {
-		"@babel/preset-env": "^7.0.0"
-	}
-}
-EOF
-npm install --save-dev
-
 # Babelify in-place for full current ESx compatiblity.
 cat << EOF > .babelrc
 {
@@ -52,8 +42,8 @@ for f in $(find assets/js -type f -name *.js); do
 done
 
 for f in $(ls assets/*.css); do
-	cssnextgen $f > $f.tmp && mv $f.tmp $f
+	css-nextgen $f > $f.tmp && mv $f.tmp $f
 	cleancss --skip-rebase $f -o $f.min && mv $f.min $f
 done
 
-rm -r node_modules package.json package-lock.json .browserslistrc .babelrc
+rm .browserslistrc .babelrc
